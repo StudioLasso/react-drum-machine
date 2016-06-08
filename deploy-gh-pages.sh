8ff5f8c65b7eaa46e41843d509f51c12ee42b68e
Paul Musso <paul.musso@orange.com>
2016-06-09 01:05:04 +0200

    chore(build): build demo with webpack and upload it gh-pages

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
-cd dist
+cd demo/dist
 git init
 git config user.name "PaulMusso"
 git config user.email "paul.musso@orange.com"
