98cef4c79bbb855e210d90665292cdd0dc2b87c9
Gilles Larroze <glarroze@gmail.com>
2017-02-15 15:18:33 +0100

    test

--- src/utils/time.js
+++ src/utils/time.js
@@ -18,8 +18,9 @@ export function getElapsedTime(playerState, time) {
 	return playerState.pausedTime || ((time - playerState.startTime) || 0);
 }
 
-export function getCurrentBeat(playerState, songState, time) {
+export function getCurrentBeat(playerState, songState, time) {	
 	const elapsedTime = getElapsedTime(playerState, time);
+	console.log(elapsedTime)
 	return Math.floor(elapsedTime / 60 * songState.bpm);
 }
 
@@ -29,7 +30,7 @@ export function getCurrentDivision(playerState, songState, time) {
 }
 
 export function getBeatSize(playerState, songState) {
-	return playerState.divisionSize * songState.divisionperbeat;	
+	return playerState.divisionSize * songState.divisionperbeat;
 }
 
 export function getMeasureSize(playerState, songState) {
